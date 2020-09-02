adb307b5cbb9ca1e52fd599df1028a189fcbee6b
Dawn DeMeo <dawn.demeo@gmail.com>
2020-09-02 09:30:19 -0400

    Completed "Day 28: RegEx, Patterns, and Intro to Databases"

---
+++ src/main/java/com/dawndemeo/thirtydaysofcode/day28regexpatternsanddb/Solution.java
@@ -0,0 +1,44 @@
+package com.dawndemeo.thirtydaysofcode.day28regexpatternsanddb;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        List<String> nameList = new ArrayList<>();
+
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+            
+            if (emailID.endsWith("gmail.com")) {
+                nameList.add(firstName);
+            }
+            
+        }
+        
+        nameList.sort(null);
+        for (String name : nameList) {
+            System.out.println(name);
+        }
+        
+        scanner.close();
+    }
+}
+
